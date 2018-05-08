3f4afdbd79cf473bf2f69ee681aaa994c71aa37f
AHendrix276 <alysonhendrix27@gmail.com>
2018-05-08 13:14:27 +0300

    Updated Login, Logout and User Session

--- appointments.php
+++ appointments.php
@@ -1,3 +1,14 @@
+<?php 
+    session_start();
+    
+    if (isset($_SESSION['current_user']) && isset($_SESSION['current_user']['id'])) {
+        // Right now we don't do anything
+    } else {
+        echo("<script>alert('Oops! Please log in to see the profile.')</script>");
+        echo("<script>window.location = 'HTML/index.html';</script>");
+        exit;
+    }
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,101 +16,104 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>24/7 Doctor | Appointments</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="../CSS/style.css" />
+    <!-- <link rel="stylesheet" type="text/css" media="screen" href="../CSS/style.css" /> -->
+    <link rel="stylesheet" type="text/css" media="screen" href="./CSS/style.css" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
   </head>  
 <body>
-<div id="navbar" class="navbarMain">
-<ul>
-    <li><a class="active" href="index.html">Home</a></li>
-    <li><a href="about.html">About</a></li>
-    <li><a href="faq.html">FAQ</a></li>
-    <li><a href="contact.html">Contact</a></li>
-</ul>
-</div>
-<div class="wrapper">
-  <main>
-    <div class="toolbar">
-      <div class="current-month">May 2018</div>
+    <div id="navbar" class="navbarMain">
+        <ul>
+            <li><a class="active" href="index.html">Home</a></li>
+            <li><a href="about.html">About</a></li>
+            <li><a href="faq.html">FAQ</a></li>
+            <li><a href="contact.html">Contact</a></li>
+        </ul>
     </div>
-      <div class="calendar">
-        <?php
-          require_once("databaseConnection.php");
-          include("dbcheck.php")
-          
-          function showEvent(){
-          
-              $final_event = "<div>";
-              $dbConn = new DatabaseConnection();
-              $pdo = $dbConn->getConnection();
+    <div class="wrapper">
+        <main>
+            <div class="toolbar">
+                <div class="current-month">May 2018</div>
+            </div>
+            <div class="calendar">
+                <div class="table100 ver2 m-b-110">
+                    <div class="table100-head">
+                        <table>
+                            <thead>
+                            <tr class="row100 head">
+                                <th class="cell100 column1">Appointment ID</th>
+                                <th class="cell100 column2">Date</th>
+                                <th class="cell100 column3">Doctor ID</th>
+                            </tr>
+                            </thead>
+                        </table>
+                    </div>
+                    <div class="table100-body js-pscroll">
+                        <table>
+                            <tbody>
+                                <?php
+                                    require_once("databaseConnection.php");
+                                    $dbConn = new DatabaseConnection();
+                                    $pdo = $dbConn->getConnection();
 
-              $sql = "SELECT * FROM events WHERE patient_id = 'patients.id;'";
-              $q = $pdo->query($sql);
-          
-              echo ('<table class="table100 ver2 m-b-110"><thead><tr class="row100 head"><th class="cell100 column1">Class name</th><th class="cell100 column2">Type</th><th class="cell100 column3">Hours</th><th class="cell100 column4">Trainer</th><th class="cell100 column5">Spots</th></tr></thead>');
-          
-              $q->setFetchMode(PDO::FETCH_ASSOC);
-              while ($r = $q->fetch()) {
-                  $appt = sprintf('Appointment id: %s "<tr class="row100 body"><td class="cell100 column1>"Date: %s "<tr class="row100 body><td class="cell100 column1>"Doctor\'s id: %s <br/>',$r['id'], $r['date'], $r['doctor_id']);
-                  echo $appt;
-          
-              echo "</table>";
-              }
-          }
-          
-          showEvent(); 
-          
-          ?>
-          <div id="show_event"></div>
-      </div>
- </div>
-  </main>
-  <sidebar>
-    <div class="logo">WELCOME</div>
-    <div class="avatar">
-      <div class="avatar__img">
-        <img src="../img/calendar.png" alt="avatar">
-      </div>
-      <!--<div class="avatar__name">John Smith</div> -->
+                                    $sql = "SELECT * FROM events WHERE patient_id = " . $_SESSION['current_user']['id'];
+                                    $q = $pdo->query($sql);
+                                    $q->setFetchMode(PDO::FETCH_ASSOC);
+
+                                    while ($r = $q->fetch()) {
+                                        echo "<tr class='row100 body'>";
+                                            echo "<td class='cell100 column1'>" . $r['id'] . "</td>";
+                                            echo "<td class='cell100 column2'>" . $r['date'] . "</td>";
+                                            echo "<td class='cell100 column3'>" . $r['doctor_id'] . "</td>";
+                                        echo "</tr>";
+                                    }
+                                ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div><!-- end .calendar -->
+        </main>
+        <sidebar>
+            <div class="logo">WELCOME</div>
+            <div class="avatar">
+            <div class="avatar__img">
+                <img src="../img/calendar.png" alt="avatar">
+            </div>
+            <!--<div class="avatar__name">John Smith</div> -->
+            </div>
+            <nav class="menu">
+            <a class="menu__item menu__item--active" href="profile.php">
+                <i class="menu__icon fa fa-envelope"></i>
+                <span class="menu__text">PROFILE</span>
+            </a>
+            <a class="menu__item" href="appointments.php">
+                <i class="menu__icon fa fa-list"></i>
+                <span class="menu__text">APPOINTMENTS</span>
+            </a>
+            <a class="menu__item menu__item" href="calendar.php">
+                <i class="menu__icon fa fa-calendar"></i>
+                <span class="menu__text">CALENDAR</span>
+            </a>
+            </nav>
+        </sidebar>
     </div>
-    <nav class="menu">
-      <a class="menu__item" href="profile.php">
-        <i class="menu__icon fa fa-home"></i>
-        <span class="menu__text">OVERVIEW</span>
-      </a>
-      <a class="menu__item menu__item--active" href="profile.php">
-        <i class="menu__icon fa fa-envelope"></i>
-        <span class="menu__text">PROFILE</span>
-      </a>
-      <a class="menu__item" href="appointments.php">
-        <i class="menu__icon fa fa-list"></i>
-        <span class="menu__text">APPOINTMENTS</span>
-      </a>
-      <a class="menu__item menu__item" href="calendar.html">
-        <i class="menu__icon fa fa-calendar"></i>
-        <span class="menu__text">CALENDAR</span>
-      </a>
-    </nav>
-  </sidebar>
-</div>
-<div id="footer" class="navbarMain">
-    <ul style="float:left"></ul>
-    <li class="copyright">Copyright (c) 2018 Medical Booking Inc.</li>
-    <ul style="float: right">
-        <li><button class="buttonFooter" data-container="body" data-toggle="popover"  data-trigger="hover" data-placement="top"
-            data-content="Subject to the express provisions of this notice:
-            (a)	we, together with our licensors, own and control all the copyright and other intellectual property rights in our website and the material on our website; and
-            (b)	all the copyright and other intellectual property rights in our website and the material on our website are reserved."
-            >Copyright Policy</button></li>
-        <li><button class="buttonFooter" data-container="body" data-toggle="popover"  data-trigger="hover" data-placement="top"
-            data-content="Our website contains general medical information. The medical information is not advice and should not be treated as such.">Disclaimer</button></li>
-    </ul>
-</div>
-<script src="../JS/script.js"></script>
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <div id="footer" class="navbarMain">
+        <ul style="float:left"></ul>
+        <li class="copyright">Copyright (c) 2018 Medical Booking Inc.</li>
+        <ul style="float: right">
+            <li><button class="buttonFooter" data-container="body" data-toggle="popover"  data-trigger="hover" data-placement="top"
+                data-content="Subject to the express provisions of this notice:
+                (a)	we, together with our licensors, own and control all the copyright and other intellectual property rights in our website and the material on our website; and
+                (b)	all the copyright and other intellectual property rights in our website and the material on our website are reserved."
+                >Copyright Policy</button></li>
+            <li><button class="buttonFooter" data-container="body" data-toggle="popover"  data-trigger="hover" data-placement="top"
+                data-content="Our website contains general medical information. The medical information is not advice and should not be treated as such.">Disclaimer</button></li>
+        </ul>
+    </div>
+    <script src="../JS/script.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 </body>
-
 </html>
--- dbcheck.php
+++ dbcheck.php
@@ -10,31 +10,36 @@
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
     }
-    $upass = $_POST['psw'];
-    $email = $_POST['email'];
-    /*$sql = "SELECT * FROM patients WHERE email ='$email'";
-    $result = $conn->query($sql);
-    $row = $result->fetch_assoc();*/
 
-    // Test if username and password match one available in the database - not working...
-    $query = "SELECT * FROM patients WHERE email='$email'";
-    $result = mysqli_query($conn, $query) or die(mysql_error());
-    $count = mysqli_num_rows($result);
-    // If a email and username match then gather the user's data
-    if($count == 1) { 
-        $sql = "SELECT * FROM patients WHERE email ='$email'";
-        $result = $conn->query($sql);
-        $row = $result->fetch_assoc();
-    } else{
-        echo("<script>alert('Oops! We did not find your email. Please try again.')</script>");
-        echo("<script>window.location = 'HTML/index.html';</script>");
-        exit;
-    }
-        // Check that the two passwords match
-    if($_POST['psw'] != $_POST['psw_r']){
-        echo("<script>alert('Whoops! Your passwords did not match. Try again!')</script>");
-        echo("<script>window.location = 'HTML/index.html';</script>");
-        exit; 
-    }
+    // 1. Check if we have email and password and they are not empty
+    if (isset($_POST['psw']) && isset($_POST['email']) && trim($_POST['psw']) != '' && trim($_POST['email']) != '') {
+        $query = "SELECT * FROM patients WHERE email = '".$_POST['email']."' AND password = '".$_POST['psw']."'";
+        $result = mysqli_query($conn, $query);
+        
+        // Check if we found the patient
+        if (mysqli_num_rows($result) > 0) {
+            // Store patient/user information in the session
+            //var_dump($result->fetch_assoc());
+            $current_user = $result->fetch_assoc();
+            
+            $_SESSION['current_user'] = $current_user;
+            //$_SESSION['current_user']['id'];
+            
+        } else {
+            echo("<script>alert('Whoops! Email and password did not match. Try again!')</script>");
+            echo("<script>window.location = 'HTML/index.html';</script>");
+            exit; 
+        }
+        
+    } else {
+        // Check if we have an active session
 
-?>
+        // Check if they are logged in. If not, send them to the login page
+        if (isset($_SESSION['current_user']) && isset($_SESSION['current_user']['id'])) {
+            // Right now we don't do anything
+        } else {
+            echo("<script>alert('Oops! Please log in to see the profile.')</script>");
+            echo("<script>window.location = 'HTML/index.html';</script>");
+            exit;
+        }
+    }

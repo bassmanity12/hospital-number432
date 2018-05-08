af03bcf98f53d024d7b9c037b80006fcd74e54fd
AHendrix276 <alysonhendrix27@gmail.com>
2018-05-08 10:12:36 +0300

    Added Alert for Username and Password Check

--- dbcheck.php
+++ dbcheck.php
@@ -1,10 +1,3 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    </head>
-    <body>
 <?php
     $servername = "ec2-18-197-115-58.eu-central-1.compute.amazonaws.com";
     $username = "group_4_user";
@@ -14,26 +7,25 @@
     $conn = mysqli_connect($servername, $username, $pass, $dbname);
 
     // Check connection
-
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
-    };
+    }
     $upass = $_POST['psw'];
-    $email = $_POST['email']
+    $email = $_POST['email'];
+    /*$sql = "SELECT * FROM patients WHERE email ='$email'";
+    $result = $conn->query($sql);
+    $row = $result->fetch_assoc();*/
+
     // Test if username and password match one available in the database - not working...
     $query = "SELECT * FROM patients WHERE email='$email'";
-    echo($query);
     $result = mysqli_query($conn, $query) or die(mysql_error());
-    $count = mysqli_num_rows($ask);
-    
+    $count = mysqli_num_rows($result);
     // If a email and username match then gather the user's data
-    if($count == 1){ 
-        header("Location: profile.php"); 
+    if($count == 1) { 
         $sql = "SELECT * FROM patients WHERE email ='$email'";
         $result = $conn->query($sql);
         $row = $result->fetch_assoc();
-    }
-    else{
+    } else{
         echo("<script>alert('Oops! We did not find your email. Please try again.')</script>");
         echo("<script>window.location = 'HTML/index.html';</script>");
         exit;
@@ -44,8 +36,5 @@
         echo("<script>window.location = 'HTML/index.html';</script>");
         exit; 
     }
-       
-?>
 
-</body>
-</html>
+?>
